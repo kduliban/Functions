d77f6df188ac5ff756caa2e7f446f120801d4da9
kasiaduliban@gmail.com <kasiaduliban@gmail.com>
2022-03-27 17:43:22 +0200

    Find if palindrom

--- is_this_a_palindrom.py
+++ is_this_a_palindrom.py
@@ -3,4 +3,10 @@ word = 'potop'
 def is_it_a_palindrom(word):
     if type (word) != str:
         print("Input is not a strong, function won't work") 
-    else: 
+    else:
+        if word == word[::-1]:
+            print(True)
+        else:
+            print(False)
+
+is_it_a_palindrom(word)
